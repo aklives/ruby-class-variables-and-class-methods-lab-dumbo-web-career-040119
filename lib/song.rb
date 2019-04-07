76fc71743bab1c23469c74ae9788b1730a976ab3
Adam Kearney <aklives@hotmail.com>
2019-04-07 02:06:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,22 @@
 class Song 
   
-  attr_accessor :name, :artist, :genre, :count, :artists, :genres
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0  # song count
+  @@artists = []
+  @@genres = []
+  
   
   def initialize(name, artist, genre)
     
     @name = name
     @artist = artist
     @genre = genre
-    @@count = 0  # song count
+    
     @@count += 1
-    @@artists = []
+    
     @@artists << artist 
-    @@genres = []
+   
     @@genres << genre
   
   end 
